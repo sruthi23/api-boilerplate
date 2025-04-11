00b4805ad295b490bf1fd2fa9a00fec3d3aad9c9
sruthi23 <sruthi@lightrains.com>
2025-04-11 22:55:01 +0530

    Update query

--- src/utils/queries.js
+++ src/utils/queries.js
@@ -1,9 +1,10 @@
 async function getAllUsers(supabase) {
-  const { data, error } = await supabase.from('users').select('*')
+  const { data, error } = await supabase
+    .from('users')
+    .select('id,email,created_at')
   if (error) throw error
   return data
 }
-
 
 module.exports = {
   getAllUsers
